318d8b8878c21cbf6152fb6bc7517e2102fb8fff
Jengopockets <jengopockets@gmail.com>
2019-08-26 14:18:33 -0400

    post register endpoint set up

---
+++ auth/auth-model.js
@@ -0,0 +1,14 @@
+const db = require('../database/dbConfig.js');
+module.exports = {
+    add,
+}
+async function add(user) {
+    const [id] = await db('users').insert(user);
+
+    return findById(id) 
+}
+function findById(id) {
+    return db('users')
+        .where({ id })
+        .first();
+}
---
+++ auth/auth-router.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const Users = require('./auth-model');
+const secrets = require('../config/secrets');
+
+router.post('/register', (req, res) => {
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 10);
+    user.password = hash;
+    
+    Users.add(user)
+    .then((saved) => {
+        res.status(201).json(saved);
+    }).catch((err) => {
+        res.status(500).json(err);
+    });
+});
+
