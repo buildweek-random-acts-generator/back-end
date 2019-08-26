f032e5df17781a17b7cb1803d617c65aa010d33b
Jengopockets <jengopockets@gmail.com>
2019-08-26 15:12:53 -0400

    Authentication endpoints set up

--- api/server.js
+++ api/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const authRouter = require('../auth/auth-router');
+
 
 
 const server = express();
@@ -9,6 +11,8 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use('/api/auth', authRouter);
 
 server.get('/', (req, res) => {
     res.send("It's alive")
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -17,4 +17,35 @@ router.post('/register', (req, res) => {
         res.status(500).json(err);
     });
 });
+router.post('/login', (req, res) => {
+    let { email, password } = req.body;
 
+    Users.findBy({ email })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                const token = getJwt(user);
+                res.status(200).json({message: `Welcome ${user.email}!`, token});
+            } else {
+                res.status(401).json({ message: 'Invalid Credentials'})
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
+
+
+function getJwt(user) {
+    const payload = {
+      subject:user.id,
+      email: user.email,
+      jwtid: 1,
+    };
+    const options = {
+      expiresIn: '8h',
+    }
+    return jwt.sign(payload, secrets.jwtSecret, options);
+  }
+  
+  module.exports = router;
